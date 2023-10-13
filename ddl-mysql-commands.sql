16739d52038da0faabd135019b69363bfe28e006
Harish_tkd <harishnik08@gmail.com>
2023-10-13 17:29:39 +0530

    foreign key ref has been added

--- ddl-mysql-commands.sql
+++ ddl-mysql-commands.sql
@@ -1,5 +1,6 @@
-CREATE databases travel_planner
+CREATE DATABASE travel_planner;
 USE travel_planner;
+
 CREATE TABLE User (
     user_id INTEGER PRIMARY KEY,
     password VARCHAR(255),
@@ -8,18 +9,20 @@ CREATE TABLE User (
 
 CREATE TABLE Trip (
     trip_id INTEGER PRIMARY KEY,
-    user_id integer,
-    acommodation_id integer , 
-    destination_id integer , 
-    start_date varchar(225),
-    end_date varchar(225)
+    user_id INTEGER,
+    accommodation_id INTEGER, 
+    destination_id INTEGER, 
+    start_date VARCHAR(225),
+    end_date VARCHAR(225),
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (accommodation_id) REFERENCES Accommodation(accommodation_id),
+    FOREIGN KEY (destination_id) REFERENCES Destination(destination_id)
 );
 
-CREATE TABLE Acommodation(
-    acommodation_id INTEGER PRIMARY KEY,
-	name TEXT,
+CREATE TABLE Accommodation (
+    accommodation_id INTEGER PRIMARY KEY,
+    name TEXT,
     address VARCHAR(255)
-   
 );
 
 CREATE TABLE Destination (
@@ -30,11 +33,10 @@ CREATE TABLE Destination (
 
 CREATE TABLE Rooms (
     room_id INTEGER PRIMARY KEY,
-    acommodation_id integer , 
-    check_in varchar(225),
-    check_out varchar(225),
-    status varchar(225),
-    no_of_beds integer
+    accommodation_id INTEGER,
+    check_in VARCHAR(225),
+    check_out VARCHAR(225),
+    status VARCHAR(225),
+    no_of_beds INTEGER,
+    FOREIGN KEY (accommodation_id) REFERENCES Accommodation(accommodation_id)
 );
-
-
