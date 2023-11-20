2cd3dd4572e01c7b864ce7668587dde314dc2450
harish08102004 <harishnik08@gmail.com>
2023-11-20 12:23:28 +0530

    changes made

--- main.py
+++ main.py
@@ -1,126 +1,163 @@
-# Define the User class
-class User:
-    def __init__(self, user_id, username, password):
-        self.user_id = user_id
-        self.username = username
-        self.password = password
-        self.trips = []  
+class BaseEntity:
+    def __init__(self, name, id_file):
+        self._id = self._get_next_id(id_file)
+        self._name = name
+
+    def _get_next_id(self, id_file):
+        try:
+            with open(id_file, "r") as file:
+                entity_id = int(file.read())
+        except FileNotFoundError:
+            entity_id = 1
+
+        with open(id_file, "w") as file:
+            file.write(str(entity_id + 1))
+
+        return entity_id
+
+    def get_id(self):
+        """Getter method for ID."""
+        return self._id
+
+    def get_name(self):
+        """Getter method for name."""
+        return self._name
 
     def __str__(self):
-        return f"User ID: {self.user_id}, Username: {self.username}"
-    
-    # Define the Budget class
-class Budget:
-    def __init__(self):
-        self.total_budget = 0
-        self.expenses = {}  
+        return f"{self.__class__.__name__} ID: {self._id}, Name: {self._name}"
 
-        self.expense_categories = ["Accommodation", "Food", "Transportation"]
 
-        for category in self.expense_categories:
-            self.expenses[category] = 0
+class User(BaseEntity):
+    def __init__(self, username, password):
+        super().__init__(username, "user_id.txt")
+        self._username = username
+        self._password = password
+        self._trips = []
 
-    def __str__(self):
-        return f"Total Budget: {self.total_budget}"
+    def get_username(self):
+        """Getter method for username."""
+        return self._username
 
-# Define the Destination class
-class Destination:
-    def __init__(self, destination_id, name, location):
-        self.destination_id = destination_id  
-        self.name = name 
-        self.location = location  
+    def get_password(self):
+        """Getter method for password."""
+        return self._password
 
-    def __str__(self):
-        return f"Destination ID: {self.destination_id}, Name: {self.name}"  
-      
-# Define the Trip class
-class Trip:
-    def __init__(self, trip_id, user, destination, start_date, end_date):
-        self.trip_id = trip_id
-        self.user = user
-        self.destination = destination
-        self.start_date = start_date
-        self.end_date = end_date
-        self.accommodations = [] 
-        self.budget = Budget()  
+    def get_trips(self):
+        """Getter method for trips."""
+        return self._trips
 
     def __str__(self):
-        return f"Trip ID: {self.trip_id}, Destination: {self.destination.name}, Start Date: {self.start_date}, End Date: {self.end_date}"
+        return f"{super().__str__()}, Username: {self._username}"
 
-class Destination:
-    def __init__(self, destination_id, name, location):
-        self.destination_id = destination_id
-        self.name = name
-        self.location = location
 
-    def __str__(self):
-        return f"Destination ID: {self.destination_id}, Name: {self.name}"
+class Destination(BaseEntity):
+    def __init__(self, name, location):
+        super().__init__(name, "destination_id.txt")
+        self._location = location
 
-class Accommodation:
-    def __init__(self, accommodation_id, name, address, check_in, check_out):
-        self.accommodation_id = accommodation_id
-        self.name = name
-        self.address = address
-        self.check_in = check_in
-        self.check_out = check_out
-        self.is_booked = False  
+    def get_location(self):
+        """Getter method for location."""
+        return self._location
 
     def __str__(self):
-        return f"Accommodation ID: {self.accommodation_id}, Name: {self.name}, Address: {self.address}"
+        return f"{super().__str__()}, Location: {self._location}"
 
-class Budget:
-    def __init__(self):
-        self.total_budget = 0
-        self.expenses = {}  
 
-        self.expense_categories = ["Accommodation", "Food", "Transportation"]
+class Room(BaseEntity):
+    next_room_id = 1
 
-        for category in self.expense_categories:
-            self.expenses[category] = 0
+    def __init__(self, no_of_beds):
+        super().__init__(f"Room-{Room.next_room_id}", "room_id.txt")
+        Room.next_room_id += 1
+        self._no_of_beds = no_of_beds
+
+    def get_no_of_beds(self):
+        """Getter method for the number of beds."""
+        return self._no_of_beds
 
     def __str__(self):
-        return f"Total Budget: {self.total_budget}"
-
-username = input("Enter your username: ")
+        return f"{super().__str__()}, Beds: {self._no_of_beds}"
 
 
-user1 = User(1, username, "password123")
-destination1 = Destination(1, destination_name, "France")
-trip1 = Trip(1, user1, destination1, start_date, end_date)
+class Accommodation(BaseEntity):
+    next_accommodation_id = 1
 
-print(user1)
-print(trip1)
-print(destination1)
+    def __init__(self, name, address):
+        super().__init__(name, "accommodation_id.txt")
+        self._address = address
+        self._rooms = []
 
-# Create an array of Destination objects
-destinations = []
-num_destinations = int(input("Enter the number of destinations you want to add: "))
+    def add_room(self, no_of_beds):
+        room = Room(no_of_beds)
+        self._rooms.append(room)
+        return room
 
-# Add destinations to the array
-for i in range(num_destinations):
-    destination_name = input(f"Enter the name for Destination {i+1}: ")
-    location = input(f"Enter the location for Destination {i+1}: ")
-    destination = Destination(i + 1, destination_name, location)
-    destinations.append(destination)
+    def get_address(self):
+        """Getter method for address."""
+        return self._address
 
-# Set the number of trips to 1
-num_trips = 1
+    def get_rooms(self):
+        """Getter method for rooms."""
+        return self._rooms
 
-# Add a trip to the array
-for i in range(num_trips):
-    destination_id = int(input("Enter the destination ID for the trip: "))
-    start_date = input("Enter the start date for the trip (DD-MM-YYYY): ")
-    end_date = input("Enter the end date for the trip (DD-MM-YYYY): ")
+    def __str__(self):
+        return f"{super().__str__()}, Address: {self._address}, Rooms: {len(self._rooms)}"
 
-    # Assuming destination_id is always valid
-    selected_destination = destinations[destination_id - 1]
 
-    trip = Trip(i + 1, user1, selected_destination, start_date, end_date)
-    user1.trips.append(trip)  # Add the trip object to the user's trips list
-    print(f"Trip to {selected_destination.name} from {start_date} to {end_date} added successfully.")
+class Trip(BaseEntity):
+    def __init__(self, user, destination, accommodation, start_date, end_date):
+        super().__init__(destination.get_name(), "trip_id.txt")
+        self._user = user
+        self._destination = destination
+        self._accommodation = accommodation
+        self._start_date = start_date
+        self._end_date = end_date
 
-# Display user and trip information
-print(f"User Information: {user1}")
-print("Trips:")
-for trip in user1.trips:
-    print(trip)
+    def get_user(self):
+        """Getter method for the user."""
+        return self._user
+
+    def get_destination(self):
+        """Getter method for the destination."""
+        return self._destination
+
+    def get_accommodation(self):
+        """Getter method for the accommodation."""
+        return self._accommodation
+
+    def get_start_date(self):
+        """Getter method for the start date."""
+        return self._start_date
+
+    def get_end_date(self):
+        """Getter method for the end date."""
+        return self._end_date
+
+    def __str__(self):
+        return f"{super().__str__()}, User: {self._user.get_name()}, Destination: {self._destination.get_name()}, Accommodation: {self._accommodation.get_name()}, Start Date: {self._start_date}, End Date: {self._end_date}"
+
+
+# Example Usage:
+
+# Create User
+user = User("JohnDoe", "password123")
+
+# Create Destination
+destination = Destination("Paris", "France")
+
+# Create Room
+room = Room(2)
+
+# Create Accommodation
+accommodation = Accommodation("Hotel XYZ", "123 Main St")
+accommodation.add_room(room.get_no_of_beds())
+
+# Create Trip
+trip = Trip(user, destination, accommodation, "2023-11-01", "2023-11-07")
+
+# Print information
+print(user)
+print(destination)
+print(room)
+print(accommodation)
+print(trip)
